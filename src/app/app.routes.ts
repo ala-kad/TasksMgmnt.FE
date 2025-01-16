d7bdf74f413082ee0d4e16bc00b02be5bf213300
ala-kad <kaddechiala@gmail.com>
2025-01-16 22:04:24 +0100

    App Component with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { TaskFormComponent } from './tasks/task-form/task-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', loadChildren: () => import('./tasks/tasks.module').then(m => m.TasksModule)},
+    { path: 'new-task', component: TaskFormComponent },
+];
